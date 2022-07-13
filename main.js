2712c76f360a7d2864ce6f5625e04251b0341b33
ecanvasser <eddiecanvassere9@gmail.com>
2022-07-12 22:06:33 -0700

    Update conditions for newGridBtn

--- main.js
+++ main.js
@@ -33,6 +33,12 @@ const newGridBtn = document.querySelector('#new-grid');
 let rowBox = document.querySelector('.row');
 let colBox = document.querySelector('.column');
 newGridBtn.addEventListener('click', function() {
-    grid.innerHTML = '';
-    newGrid(rowBox.value, colBox.value);
+    if (rowBox.value > 100 || colBox.value > 100) {
+        alert('Max grid size is 100 x 100. Use smaller size')
+    } else if (!(rowBox.value > 0) || !(colBox.value > 0)) {
+        alert('Please use numbers only');
+    } else {
+        grid.innerHTML = '';
+        newGrid(Math.ceil(rowBox.value), Math.ceil(colBox.value));
+    }
 })
